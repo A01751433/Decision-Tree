dfb0bf9fcc24eefb84aebd7bab9271ec7fc54860
A01751433 <A01751433@tec.mx>
2026-09-09 09:53:14 -0600

    Agregé tarea con framework
    Decidí que en este mismo github voy a tener las dos tareas, cada una en su propia carpeta

---
+++ Con Framework/random_forest.py
@@ -0,0 +1,7 @@
+"""
+Este es mi archivo para la entrega:
+'Momento de Retroalimentación: Módulo 2 Uso de framework o biblioteca de aprendizaje máquina
+para la implementación de una solución. (Portafolio Implementación)'
+
+Decidí implementar Random Forest
+"""
